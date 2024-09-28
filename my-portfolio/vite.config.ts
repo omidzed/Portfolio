2922d2e5c7bfe7eebc9a5cea0f6b64ef93279a56
Omid Asadi <omid.develops@gmail.com>
2024-09-28 08:58:44 -0700

    fixing rolleupOptions

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
 	optimizeDeps: {
 		include: ['lodash/throttle'],
 	},
+	build: {
+		rollupOptions: {
+			external: source => {
+				if (source === 'lodash/throttle') {
+					return false; // Do not treat lodash/throttle as external
+				}
+				return true; // Treat other modules as external as per default behavior
+			},
+		},
+	},
 });
